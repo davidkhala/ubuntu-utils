3e6c478a3c7a2d32ad540881eb19b63e333297dd
davidLiu <david-khala@hotmail.com>
2018-12-06 14:56:15 +0800

    add bash template

---
+++ bashTemplate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+CURRENT=$(cd $(dirname ${BASH_SOURCE}) && pwd)
+
+fcn=$1
+remain_params=""
+for ((i = 2; i <= ${#}; i++)); do
+	j=${!i}
+	remain_params="$remain_params $j"
+done
