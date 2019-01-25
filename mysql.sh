ed82c5e81cc7ebe11fc4085e30185b79fd3c8b62
davidliu <david-khala@hotmail.com>
2019-01-25 16:35:13 +0800

    add mysql change password

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,16 @@
-function install(){
-    sudo apt install mysql-server
+remain_params=""
+for ((i = 2; i <= $#; i++)); do
+	j=${!i}
+	remain_params="$remain_params $j"
+done
+function install() {
+	sudo apt install mysql-server
 }
-$1
+function start() {
+	systemctl start mysql
+}
+function setRootPassword() {
+    echo "targeted new password [$1]"
+	mysql -u root -p -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$1'"
+}
+$1 $remain_params
