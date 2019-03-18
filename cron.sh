e862d97f2e30a14c58a8362e2ec3c35922ae0347
davidLiu <david-khala@hotmail.com>
2019-03-18 16:57:56 +0800

    add cron helper

---
+++ cron.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+fcn="$1"
+remain_params=""
+for ((i = 2; i <= $#; i++)); do
+	j=${!i}
+	remain_params="$remain_params $j"
+done
+sysCronTab="/etc/crontab"
+function addJob() {
+    local newLine=$1
+    echo "*  *	* * *	root	$newLine" | sudo tee -a $sysCronTab
+    sudo /etc/init.d/cron restart
+}
+$fcn $remain_params
+
