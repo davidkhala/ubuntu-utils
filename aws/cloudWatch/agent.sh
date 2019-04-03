ffb1bd17324238846469312637b9621edf57405b
davidLiu <david-khala@hotmail.com>
2019-04-03 12:02:45 +0800

    new cloudWatchAgent

---
+++ aws/cloudWatch/agent.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+agentCommonConfigPath="/opt/aws/amazon-cloudwatch-agent/etc/common-config.toml"
+
+install(){
+    wget "https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb"
+    sudo apt install ./amazon-cloudwatch-agent.deb
+}
+configGen(){
+    sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+    echo config.json will be created at "\$PWD": $PWD
+}
+start(){
+    # TODO is it for ec2 only? 
+    sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:${configurationFilePath} -s
+}
