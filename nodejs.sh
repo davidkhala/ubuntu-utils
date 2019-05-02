00135d35e28432bcd46328f6206f98ba6c07e98e
davidLiu <david-khala@hotmail.com>
2019-05-02 14:26:47 +0800

    add packageLock

--- nodejs.sh
+++ nodejs.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+remain_params=""
+for ((i = 2; i <= $#; i++)); do
+    j=${!i}
+    remain_params="$remain_params $j"
+done
 install8() {
     curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
     sudo apt-get -qq install -y nodejs
 }
-$1
+packageLock() {
+    local CMD="npm config set package-lock $1"
+    echo $CMD
+    $CMD
+}
+$1 $remain_params
