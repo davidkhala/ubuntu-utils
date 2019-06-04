72bab1cba5a1f8accde320e738b1f542ab48e641
davidliu <david-khala@hotmail.com>
2019-06-04 20:48:59 +0800

    add ssh shell

--- deb/shadowSocks/install.sh
+++ deb/shadowSocks/install.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 addSource() {
-    echo "deb https://repo.debiancn.org/ testing main" | sudo tee /etc/apt/sources.list.d/debiancn.list
+    echo "deb https://repo.debiancn.org/ stretch main" | sudo tee /etc/apt/sources.list.d/debiancn.list
     wget https://repo.debiancn.org/pool/main/d/debiancn-keyring/debiancn-keyring_0~20161212_all.deb -O /tmp/debiancn-keyring.deb
     sudo apt install /tmp/debiancn-keyring.deb
     sudo apt update
     rm /tmp/debiancn-keyring.deb
 }
-install(){
+install() {
     sudo apt install shadowsocks-qt5
 }
-wiki(){
+wiki() {
     echo https://github.com/debiancn/repo
 }
-$1
+if [[ -z "$1" ]]; then
+    install
+else
+    $1
+fi
--- ssh.sh
+++ ssh.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
-
+set -e
 sshPass() {
     if ! sshPass >/dev/null 2>&1; then
         sudo apt install sshpass
     fi
 }
+genRSA() {
+    local keySize
+    local email
+    local rsaKeyPrv
+    read -p "enter email (default:david-khala@hotmail.com):" email
+    read -p "enter keySize (default:4096) :" keySize
+    keySize=${keySize:-4096}
+    email=${email:-"david-khala@hotmail.com"}
+    ssh-keygen -t rsa -b $keySize -C $email
+    eval "$(ssh-agent -s)"
+
+    read -p "Enter key file path again ($HOME/.ssh/id_rsa):" rsaKeyPrv
+    rsaKeyPrv=${rsaKeyPrv:-"$HOME/.ssh/id_rsa"}
+    ssh-add $rsaKeyPrv
+}
+copyRSAPub(){
+    sudo apt install -y xclip
+    xclip -sel clip < ~/.ssh/id_rsa.pub
+}
+$1
