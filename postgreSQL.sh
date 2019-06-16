48201188e2dc4a2354aaf2a8e2353e99cce4f491
davidliu <david-khala@hotmail.com>
2019-06-16 22:59:14 +0800

    install

---
+++ postgreSQL.sh
@@ -0,0 +1,8 @@
+install(){
+    local osCode=$(lsb_release --codename --short)
+    echo "deb http://apt.postgresql.org/pub/repos/apt/ ${osCode}-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+    sudo apt-get update
+    sudo apt install postgresql-10
+}
+$1
