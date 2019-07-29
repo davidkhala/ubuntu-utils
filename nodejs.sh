ae3de090bf7868320ea4e05070160a7da78cba76
david liu <david-khala@hotmail.com>
2019-07-30 00:06:47 +0800

    missing commit

--- nodejs.sh
+++ nodejs.sh
@@ -8,11 +8,11 @@ install8() {
     curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
     sudo apt-get -qq install -y nodejs
 }
-install10(){
+install10() {
     curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
     sudo apt-get -qq install -y nodejs
 }
-install12(){
+install12() {
     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
     sudo apt-get -qq install -y nodejs
 }
