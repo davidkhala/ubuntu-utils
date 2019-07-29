d1a042dd7bab1b58a073a1fa3c57a124df377e38
david liu <david-khala@hotmail.com>
2019-07-29 23:28:52 +0800

    allow inline run like curl with bash

--- ssh.sh
+++ ssh.sh
@@ -9,14 +9,19 @@ genRSA() {
     local keySize
     local email
     local rsaKeyPrv
-    read -p "enter email (default:david-khala@hotmail.com):" email
-    read -p "enter keySize (default:4096) :" keySize
+    local inline=$1
+    if [ -z "$inline" ]; then
+        read -p "enter email (default:david-khala@hotmail.com):" email
+        read -p "enter keySize (default:4096) :" keySize
+    fi
     keySize=${keySize:-4096}
     email=${email:-"david-khala@hotmail.com"}
     ssh-keygen -t rsa -b $keySize -C $email
     eval "$(ssh-agent -s)"
 
-    read -p "Enter key file path again ($HOME/.ssh/id_rsa):" rsaKeyPrv
+    if [ -z "$inline" ]; then
+        read -p "Enter key file path again ($HOME/.ssh/id_rsa):" rsaKeyPrv
+    fi
     rsaKeyPrv=${rsaKeyPrv:-"$HOME/.ssh/id_rsa"}
     ssh-add $rsaKeyPrv
 }
