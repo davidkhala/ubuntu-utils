8436f1d8af3f2108d6b2c3e0a1aff7fe928e7519
david <david-khala@hotmail.com>
2019-07-30 16:53:49 +0800

    add git:prepare for global setup

--- git.sh
+++ git.sh
@@ -4,3 +4,10 @@ upgrade() {
     sudo apt update
     sudo apt install git -y
 }
+prepare() {
+    email=${email:-"david-khala@hotmail.com"}
+    name=${name:-"david liu"}
+    git config --global user.email $email
+    git config --global user.name $name
+}
+$1
