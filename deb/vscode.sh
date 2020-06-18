90e34f25b4f3032864c0ffd3a438023cf3a2672b
davidliu <david-khala@hotmail.com>
2020-06-18 03:38:08 +0000

    use inline instead of file buffer

--- deb/vscode.sh
+++ deb/vscode.sh
@@ -1,5 +1,4 @@
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc -o microsoft.asc # merge lines
-sudo apt-key add microsoft.asc
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"  | sudo tee /etc/apt/sources.list.d/vscode.list
 
 sudo apt update
