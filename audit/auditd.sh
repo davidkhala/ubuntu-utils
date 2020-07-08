72d5213b7d159119a1c291e687bf9c48da06d90f
david <david-khala@hotmail.com>
2020-07-09 00:01:30 +0800

    [WIP] test on clean environment

--- audit/auditd.sh
+++ audit/auditd.sh
@@ -1,9 +1,33 @@
-install(){
+set -e
+install() {
   sudo apt-get install auditd audispd-plugins
-  # what is audispd-plugins
+  # audispd-plugins:These plugins can do things like relay events to remote machines or analyze events for suspicious behavior.
 }
-start(){
+start() {
+
+  # A boot param of audit=1 should be added to ensure that all processes that run before the audit daemon starts is marked as auditable by the kernel.
+  # Not doing that will make a few processes impossible to properly audit.
   service auditd start
+}
+
+config=/etc/audit/auditd.conf # configuration file for audit daemon
+rules=/etc/audit/audit.rules  # audit rules
+
+auditDocker() {
+
+  sudo auditctl -w /usr/bin/docker
+  sudo auditctl -w /usr/bin/dockerd
+  sudo auditctl -w /usr/bin/docker-init
+  sudo auditctl -w /usr/bin/docker-proxy
 
 }
-$1
+terminateTTY(){
+  local pid=$(ps -a | grep auditd | awk '{print $1}') # The pid for service 
+  echo pid=$pid
+  sudo kill -15 $pid
+}
+listRules() {
+  sudo auditctl -l
+}
+
+$@
