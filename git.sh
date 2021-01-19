738a0a03fdd4b1d8b114172a6eb8953f2d4ede0b
David Liu <david.yx.liu@oracle.com>
2021-01-19 22:04:54 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,27 +1,9 @@
 #!/usr/bin/env bash
 set -e
-remain_params=""
-for ((i = 2; i <= ${#}; i++)); do
-    j=${!i}
-    remain_params="$remain_params $j"
-done
+
 upgrade() {
     sudo add-apt-repository ppa:git-core/ppa -y
     sudo apt update
     sudo apt install git -y
-}
-prepare() {
-    email=${email:-"david-khala@hotmail.com"}
-    name=${name:-"david liu"}
-    git config --global user.email $email
-    git config --global user.name $name
-}
-switchBranch() {
-    local branchName=$1
-    echo "switching to branch origin/$branchName"
-    git checkout -b $branchName
-    git fetch
-    git branch --set-upstream-to=origin/$branchName $branchName
-    git pull
 }
 $@
