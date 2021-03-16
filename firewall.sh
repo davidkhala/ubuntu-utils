994ad05fa877174f5da51d1f0c641022ee88743c
David Liu <david.yx.liu@oracle.com>
2021-03-16 23:10:57 +0800

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,4 +1,17 @@
 status(){
   sudo ufw status
 }
+enable(){
+  sudo ufw enable
+}
+allowAll(){
+  sudo ufw allow from 0.0.0.0/0
+}
+disable(){
+  sudo ufw disable
+}
+reload(){
+  sudo ufw reload
+}
+
 $@
