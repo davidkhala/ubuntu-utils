1944fafb06785cc96c985f0ef226e21c9c4d24ca
David Liu <david.yx.liu@oracle.com>
2021-03-17 18:19:03 +0800

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,17 +1,8 @@
-status(){
-  sudo ufw status
+reset(){
+  ## Reference: https://stackoverflow.com/questions/54794217/opening-port-80-on-oracle-cloud-infrastructure-compute-node/54810101#54810101
+  sudo iptables --flush
+  
 }
-enable(){
-  sudo ufw enable
-}
-allowAll(){
-  sudo ufw default allow incoming
-}
-disable(){
-  sudo ufw disable
-}
-reload(){
-  sudo ufw reload
-}
+
 
 $@
