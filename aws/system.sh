c38ab01e49f2680839aeb17f3ba6398cb8935d39
David Liu <david.yx.liu@oracle.com>
2021-03-29 11:45:57 +0800

    Update system.sh

--- aws/system.sh
+++ aws/system.sh
@@ -1,1 +1,7 @@
-/dev/nvme0n1p1 
+## Reference: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/recognize-expanded-volume-linux.html
+extendFileSystem() {
+    # sudo growpart /dev/xvda 1
+    sudo resize2fs /dev/nvme0n1p1
+}
+$@
+ 
