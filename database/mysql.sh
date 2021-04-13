84c202d93fd685155027f054506f456798be5516
David Liu <david.yx.liu@oracle.com>
2021-04-13 11:08:45 -0400

    Update mysql.sh

--- database/mysql.sh
+++ database/mysql.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 set -e
-remain_params=""
-for ((i = 2; i <= $#; i++)); do
-	j=${!i}
-	remain_params="$remain_params $j"
-done
 install() {
 	if ! mysql --version; then
 		sudo apt install mysql-server
@@ -36,4 +31,4 @@ setRootPassword() {
 connectionPoolSize() {
 	sudo mysql -u root -p -e 'SHOW VARIABLES LIKE "max_connections"'
 }
-$1 $remain_params
+$@
