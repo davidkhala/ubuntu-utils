92ceeacb4ca36fb2704fb8df7e6c300bb87f917c
David Liu <david.yx.liu@oracle.com>
2021-05-12 21:55:53 -0400

    Delete system.sh

--- aws/system.sh
+++
@@ -1,8 +0,0 @@
-## Reference: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/recognize-expanded-volume-linux.html
-set -e
-extendFileSystem() {
-    sudo growpart /dev/nvme0n1 1
-    sudo resize2fs /dev/nvme0n1p1
-}
-$@
- 
