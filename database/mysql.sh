10a5b6e4874c6e5f5d9252446826a60802594acc
David Liu <david.yx.liu@oracle.com>
2021-06-21 02:42:20 -0400

    Update mysql.sh

--- database/mysql.sh
+++ database/mysql.sh
@@ -30,21 +30,10 @@ setup() {
 	start
 }
 setRootPassword() {
-	echo "targeted new password [$1]"
-	local passwordOpt="-p"
-
-	if [[ -n "$2" ]]; then
-		if [[ "$2" == "--init" ]]; then
-			passwordOpt=""
-		else
-			passwordOpt="--password=$2"
-		fi
-	fi
-
-	sudo mysql -u root ${passwordOpt} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$1'"
-	sudo systemctl restart mysql
+	curl https://raw.githubusercontent.com/davidkhala/mysql-toolset/main/mysql.sh | bash -s setRootPassword $1 $2
 }
 connectionPoolSize() {
-	sudo mysql -u root -p -e 'SHOW VARIABLES LIKE "max_connections"'
+	curl https://raw.githubusercontent.com/davidkhala/mysql-toolset/main/mysql.sh | bash -s connectionPoolSize
+	
 }
 $@
