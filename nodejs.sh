e28cdd7bc73080cc3039a872bd3df749af70dd9a
David Liu <david.yx.liu@oracle.com>
2021-08-11 18:14:10 +0800

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -12,6 +12,11 @@ install12() {
     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
     sudo apt-get -qq install -y nodejs
 }
+install(){
+    sudo apt-get -qq install -y nodejs
+    node --version # current 10.19.0
+    
+}
 installLTS(){
     curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
     sudo apt-get -qq install -y nodejs
