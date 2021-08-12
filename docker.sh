15ca82e6eb3a0ae26a5dcd17be0fe61f55c502f0
David Liu <david.yx.liu@oracle.com>
2021-08-12 15:10:30 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,4 +1,9 @@
 install(){
   sudo apt -qq install -y docker.io
 }
+installCompose(){
+  
+  sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+}
 $@
