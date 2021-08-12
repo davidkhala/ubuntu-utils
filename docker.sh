33522cbf81c90877d544918cbe6687ceac8d596a
David Liu <david.yx.liu@oracle.com>
2021-08-12 15:20:14 +0800

    Update docker.sh
    Signed-off-by: DavidLiu <david.yx.liu@oracle.com>

--- docker.sh
+++ docker.sh
@@ -1,9 +1,10 @@
-install(){
-  sudo apt -qq install -y docker.io
+install() {
+  curl -sSL https://get.docker.com/ | sh
+  sudo apt-get install -y uidmap
+  dockerd-rootless-setuptool.sh install
 }
-installCompose(){
-  
+installCompose() {
   sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
-	sudo chmod +x /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
 }
 $@
