b6a346955be8d6eb5abd483d41637885bc5e93a4
David Liu <david.yx.liu@oracle.com>
2021-09-10 06:22:31 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,7 +4,6 @@ install() {
   dockerd-rootless-setuptool.sh install
 }
 installCompose() {
-  sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+  sudo apt install -y docker-compose
 }
 $@
