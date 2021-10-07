087da5312d1584da5b57f63d08c51ed452531edb
David Liu <david.yx.liu@oracle.com>
2021-10-07 04:37:54 -0400

    Create cli.sh

---
+++ gcp/cli.sh
@@ -0,0 +1,10 @@
+set -e
+setup(){
+  ## https://cloud.google.com/sdk/docs/install#deb
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+  sudo apt-get install -y apt-transport-https ca-certificates gnupg
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+  sudo apt-get update && sudo apt-get install -y google-cloud-sdk
+
+}
+$1
