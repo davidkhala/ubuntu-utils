b568f56bbfb52246d4c9876340d09a32cc4d9ec1
David Liu <david.yx.liu@oracle.com>
2021-10-29 15:04:49 +0800

    Update mysql.sh

--- database/mysql.sh
+++ database/mysql.sh
@@ -10,6 +10,15 @@ install() {
 	fi
 }
 
+installShell(){
+  wget https://repo.mysql.com/mysql-apt-config_0.8.18-1_all.deb
+  sudo apt-get install ./mysql-apt-config_0.8.18-1_all.deb
+  rm mysql-apt-config_0.8.18-1_all.deb
+  sudo apt-get update
+  sudo apt-get install -y mysql-shell
+
+}
+
 installRouter() {
 	sudo apt install -y mysql-router-community
 }
