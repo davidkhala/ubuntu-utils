a6130740224e777256b2631824af555ebcbbad5d
David Liu <david.yx.liu@oracle.com>
2022-01-03 11:54:32 +0800

    Update install.sh

--- github/install.sh
+++ github/install.sh
@@ -1,4 +1,10 @@
 hub() {
 	sudo snap install hub --classic
 }
+gh(){
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+	sudo apt update
+	sudo apt install gh
+}
 $1
