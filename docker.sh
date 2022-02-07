419f07d93c94679867f6134318a386d8a3975c77
David Liu <david.yx.liu@oracle.com>
2022-02-07 16:19:44 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,6 +3,10 @@ install() {
   sudo apt-get install -y uidmap
   dockerd-rootless-setuptool.sh install
 }
+install2(){
+  sudo snap install docker
+}
+
 installCompose() {
   sudo apt install -y docker-compose
 }
