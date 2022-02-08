bc1cc286fd478771c1dd9ad1f0cffef5ad27b98c
David Liu <david.yx.liu@oracle.com>
2022-02-08 10:40:15 +0800

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-install8() {
-    curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-    sudo apt-get -qq install -y nodejs
-}
-install10() {
-    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-    sudo apt-get -qq install -y nodejs
-}
+
 install12() {
     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
     sudo apt-get -qq install -y nodejs
@@ -18,7 +11,7 @@ install(){
     
 }
 installLTS(){
-    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
     sudo apt-get -qq install -y nodejs
 }
 $@
