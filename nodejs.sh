85b525b6eb54004ed09e6c57259b78a71c51a8cc
David Liu <david.yx.liu@oracle.com>
2022-05-13 06:04:54 -0400

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
 
-install12() {
-    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-    sudo apt-get -qq install -y nodejs
-}
 install(){
     sudo apt-get -qq install -y nodejs
     node --version # v12.22.9 (ubuntu 22)
     
 }
 installLTS(){
-    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-    sudo apt-get -qq install -y nodejs
+    curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+    sudo apt-get install -y nodejs
     sudo npm install -g npm@latest
 }
 $@
