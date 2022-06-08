a055d1a87e72f53e4970e22cb1d53b6eaa9c2d54
David Liu <david.yx.liu@oracle.com>
2022-06-08 23:07:44 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,6 +2,7 @@ install() {
   curl -sSL https://get.docker.com/ | sh
   sudo apt-get install -y uidmap
   dockerd-rootless-setuptool.sh install
+  echo "export DOCKER_HOST=unix:///run/user/$UID/docker.sock" >> ~/.bashrc
 }
 install2(){
   sudo snap install docker
