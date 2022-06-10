a8fe2ac001ede8c378985eb2467b5f6612249b5c
David Liu <david.yx.liu@oracle.com>
2022-06-10 16:28:38 +0800

    Create install.sh

---
+++ database/oracle/instantclient/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+sudo apt install -y alien
+SQLDeveloper(){
+    wget https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/Oracle-Database-Development/o/sqldeveloper-20.4.0.379.2205-20.4.0-379.2205.noarch.rpm
+    sudo alien -i --scripts sqldeveloper-*.rpm 
+    rm sqldeveloper-*.rpm
+
+}
+SQLPlus(){
+    wget https://download.oracle.com/otn_software/linux/instantclient/oracle-instantclient-sqlplus-linuxx64.rpm
+    sudo alien -i --scripts oracle-instantclient-sqlplus*.rpm 
+    rm oracle-instantclient-sqlplus*.rpm
+}
+
+instantclient(){
+    wget https://download.oracle.com/otn_software/linux/instantclient/oracle-instantclient-basic-linuxx64.rpm
+    sudo alien -i --scripts oracle-instantclient-basic*.rpm 
+    rm oracle-instantclient-basic*.rpm
+}
+
+$@
