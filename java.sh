c8982f5cdd22439ead1d6c91a42161d31101656d
David Liu <david.yx.liu@oracle.com>
2022-08-14 00:24:38 +0800

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -e
 install11(){
-    sudo add-apt-repository -y ppa:linuxuprising/java
+    	sudo add-apt-repository -y ppa:linuxuprising/java
 	sudo apt update
 	sudo apt install -y oracle-java11-installer
 	sudo apt install -y oracle-java11-set-default
+}
+install-open(){
+	sudo apt install -y default-jdk
 }
 $@
