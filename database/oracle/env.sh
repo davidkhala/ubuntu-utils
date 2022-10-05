affffc18fc8ed76b826e77b7cf1c1788dc967300
David Liu <david.yx.liu@oracle.com>
2022-10-05 22:32:29 +0800

    Create env.sh

---
+++ database/oracle/env.sh
@@ -0,0 +1,16 @@
+set -e
+
+base=$(basename /opt/oracle/instantclient*)
+if [ -z "$LD_LIBRARY_PATH" ]; then
+  export LD_LIBRARY_PATH=/opt/oracle/${base}
+else
+  export LD_LIBRARY_PATH=/opt/oracle/${base}:$LD_LIBRARY_PATH
+fi
+
+
+# sudo sh -c "echo /opt/oracle/${base} > /etc/ld.so.conf.d/oracle-instantclient.conf"
+# sudo ldconfig
+# TODO what is that?
+
+echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
+
