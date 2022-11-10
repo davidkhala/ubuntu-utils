43b89466412b8ba4f506ab773f6063a824398d42
David Liu <david.yx.liu@oracle.com>
2022-11-10 18:09:32 +0000

    non-root-ubuntu-image

--- database/oracle/install.sh
+++ database/oracle/install.sh
@@ -2,21 +2,21 @@
 set -e
 
 sudo apt install -y alien
-SQLDeveloper(){
+SQLDeveloper() {
     wget https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/Oracle-Database-Development/o/sqldeveloper-20.4.0.379.2205-20.4.0-379.2205.noarch.rpm
-    sudo alien -i --scripts sqldeveloper-*.rpm 
+    sudo alien -i --scripts sqldeveloper-*.rpm
     rm sqldeveloper-*.rpm
 
 }
-SQLPlus(){
+SQLPlus() {
     wget https://download.oracle.com/otn_software/linux/instantclient/oracle-instantclient-sqlplus-linuxx64.rpm
-    sudo alien -i --scripts oracle-instantclient-sqlplus*.rpm 
+    sudo alien -i --scripts oracle-instantclient-sqlplus*.rpm
     rm oracle-instantclient-sqlplus*.rpm
 }
 
-instant-client-lite(){
+instant-client-lite() {
     wget https://download.oracle.com/otn_software/linux/instantclient/oracle-instantclient-basiclite-linuxx64.rpm
-    sudo alien -i --scripts oracle-instantclient-basic*.rpm 
+    sudo alien -i --scripts oracle-instantclient-basic*.rpm
     rm oracle-instantclient-basic*.rpm
 }
 
