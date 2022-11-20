bac74eb4051a42476397d63789b091593eb65666
David Liu <david.yx.liu@oracle.com>
2022-11-20 17:30:43 +0800

    Update network.sh

--- system/network.sh
+++ system/network.sh
@@ -2,11 +2,15 @@
 install-netstat() {
   sudo apt install net-tools
 }
-reset-firewall() {
+allow-ssh() {
+  sudo /usr/sbin/iptables-legacy -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+}
+
+reload-firewall() {
   ## Reference: https://stackoverflow.com/questions/54794217/opening-port-80-on-oracle-cloud-infrastructure-compute-node/54810101#54810101
   sudo iptables --flush
-
 }
+
 install-firewalld() {
   echo [WARN] firewalld will conflicts with docker interface and make ssh unavailable.
   sudo apt -y install firewalld
