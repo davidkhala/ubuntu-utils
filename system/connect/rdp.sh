acb6ca75d1f436fabf033d5e0ad2bad190b4b13e
David Liu <david.yx.liu@oracle.com>
2022-11-23 15:59:05 +0800

    Update rdp.sh

--- system/connect/rdp.sh
+++ system/connect/rdp.sh
@@ -1,4 +1,7 @@
 install(){
+  # First you need a vnc-server as dependency
+  curl https://raw.githubusercontent.com/davidkhala/ubuntu-utils/master/system/connect/vnc.sh | bash -s install-server
+  
   sudo apt install -y xrdp
   sudo systemctl enable --now xrdp
 
