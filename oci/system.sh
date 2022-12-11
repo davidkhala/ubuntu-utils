3d8ea2de71f6c02dc8816ded48455c0bcf2b5ff3
David Liu <david.yx.liu@oracle.com>
2022-12-11 11:19:43 +0800

    Update system.sh

--- oci/system.sh
+++ oci/system.sh
@@ -1,7 +1,9 @@
 set -e -x
 extendDisk() {
+    #   Rescan 
     sudo dd iflag=direct if=/dev/oracleoci/oraclevda of=/dev/null count=1
     echo "1" | sudo tee /sys/class/block/`readlink /dev/oracleoci/oraclevda | cut -d'/' -f 2`/device/rescan
+    #   Extend the partition
     set +e
     curl https://raw.githubusercontent.com/oracle/oci-utils/master/libexec/oci-growfs | sudo bash
     set -e
