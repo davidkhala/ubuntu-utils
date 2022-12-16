e055731463121b9bae6c3ac562935dc012daebee
David Liu <david-khala@hotmail.com>
2022-12-16 18:56:42 +0800

    [WIP] cleaning

--- cn/install.sh
+++ cn/install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-addSource() {
+add-source() {
     echo "deb https://repo.debiancn.org/ stretch main" | sudo tee /etc/apt/sources.list.d/debiancn.list
     wget https://repo.debiancn.org/pool/main/d/debiancn-keyring/debiancn-keyring_0~20161212_all.deb -O /tmp/debiancn-keyring.deb
     sudo apt install /tmp/debiancn-keyring.deb
     sudo apt update
     rm /tmp/debiancn-keyring.deb
 }
-install() {
+install-shadowsocks() {
     sudo apt install shadowsocks-qt5
 }
 wiki() {
