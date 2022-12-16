83a6b83cb501c23a3f3c0d6a9147a38b2aac2bae
David Liu <david.yx.liu@oracle.com>
2022-12-16 21:06:53 +0800

    Update system.sh

--- oci/system.sh
+++ oci/system.sh
@@ -1,5 +1,5 @@
 set -e -x
-extendDisk() {
+extend-disk() {
     #   Rescan 
     sudo dd iflag=direct if=/dev/oracleoci/oraclevda of=/dev/null count=1
     echo "1" | sudo tee /sys/class/block/`readlink /dev/oracleoci/oraclevda | cut -d'/' -f 2`/device/rescan
@@ -7,7 +7,7 @@ extendDisk() {
     sudo growpart /dev/sda 1
     sudo resize2fs /dev/sda1
 }
-viewDisk(){
+view-disk(){
     df -hT
     lsblk
 }
