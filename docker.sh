82d426838a5e329fed81abd489c0619787f8dfbf
David Liu <david-khala@hotmail.com>
2022-12-31 22:32:10 +0800

    Fix CI appliance

--- docker.sh
+++ docker.sh
@@ -9,7 +9,9 @@ install-desktop() {
   # ubuntu server installation error: after installation, run `docker ps`
   #   internal error, please report: running "docker" failed: transient scope could not be started, job /org/freedesktop/systemd1/job/46 finished with result failed
   sudo snap install docker
-  
+}
+remove-desktop() {
+  sudo snap remove docker
 }
 
 install-compose() {
