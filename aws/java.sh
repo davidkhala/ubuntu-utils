35da4918617fc411dc4b8d02415fda8c8ce87b97
David Liu <david.yx.liu@oracle.com>
2023-01-29 23:13:01 +0800

    Delete java.sh

--- aws/java.sh
+++
@@ -1,6 +0,0 @@
-install-corretto(){
-  wget https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.deb
-  sudo apt-get install -y ./amazon-corretto-17-x64-linux-jdk.deb
-  rm amazon-corretto-17-x64-linux-jdk.deb
-}
-$@
