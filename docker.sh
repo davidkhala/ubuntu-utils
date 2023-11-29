3e59d55f10b2e0921d57a1953b2f9ba98ac3f1cd
David Liu <david-khala@hotmail.com>
2023-11-29 20:56:12 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,8 +1,10 @@
+#!/bin/bash -i 
+# https://askubuntu.com/questions/64387/cannot-successfully-source-bashrc-from-a-shell-script
 install() {
   sudo apt-get install -y uidmap
   echo "export PATH=$HOME/bin:$PATH" >> ~/.bashrc
   curl https://raw.githubusercontent.com/davidkhala/linux-utils/main/apps/docker.sh | bash -s install-rootless
-  source ~/.bashrc
+  
   
 }
 install-desktop() {
