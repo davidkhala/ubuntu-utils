9f6cea6751fbaa5bd66818025d63fad663d8823f
David Liu <david-khala@hotmail.com>
2023-11-30 10:58:42 +0800

    Update nodejs.sh

--- language/nodejs.sh
+++ language/nodejs.sh
@@ -10,7 +10,7 @@ install-LTS() {
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
     NODE_MAJOR=20
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-    
+    sudo apt update
     sudo apt-get install -y nodejs npm
 }
 uninstall-LTS(){
