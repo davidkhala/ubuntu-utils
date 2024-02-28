32e2718cbd165846b0baf40a3c93e5bab1e981b2
David Liu <david-khala@hotmail.com>
2024-02-28 11:24:21 +0800

    Update docker.sh

--- apps/docker.sh
+++ apps/docker.sh
@@ -6,7 +6,7 @@ install() {
   echo "export PATH=$HOME/bin:$PATH" >> ~/.bashrc
   curl https://raw.githubusercontent.com/davidkhala/linux-utils/main/apps/docker.sh | bash -s install-rootless
   
-  export PATH=$HOME/bin:$PATH # workaround for ubuntu ~/.bashrc
+  export PATH=$HOME/bin:$PATH # workaround for ubuntu ~/.bashrc # make this function needed to be called by `source`
   
 }
 install-desktop() {
