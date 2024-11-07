d1c13a60356e27cbec7ac33dcba28bd9c141dcef
David Liu <david-khala@hotmail.com>
2024-11-07 05:02:31 +0000

    format

--- database/mssql/install.sh
+++ database/mssql/install.sh
@@ -1,21 +1,21 @@
-repos(){
-# For Ubuntu versions >= 24.04
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+repos() {
+  # For Ubuntu versions >= 24.04
+  curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
 
-# Add repo to apt sources
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+  # Add repo to apt sources
+  curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
 
-sudo apt-get update
+  sudo apt-get update
 }
-odbc (){
-# Install the driver
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+odbc() {
+  # Install the driver
+  sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
 }
-sqlcmd(){
+sqlcmd() {
   # optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
-echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
-source ~/.bashrc
+  sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+  echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >>~/.bashrc
+  source ~/.bashrc
 
 }
 
