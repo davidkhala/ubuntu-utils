0648970d4cd1b733dca9385dd47eb07f73f9f89f
David Liu <david-khala@hotmail.com>
2024-11-07 05:13:44 +0000

    islegacy params

--- database/mssql/install.sh
+++ database/mssql/install.sh
@@ -1,6 +1,14 @@
 repos() {
-  # For Ubuntu versions >= 24.04
-  curl https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+
+  local isLegacy=$1
+  if [[ "--isLegacy" == $isLegacy ]]; then
+    # For Ubuntu versions < 24.04
+    curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+  else
+    # For Ubuntu versions >= 24.04
+    curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+
+  fi
 
   # Add repo to apt sources
   curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
@@ -8,12 +16,12 @@ repos() {
   sudo apt-get update
 }
 odbc() {
-  repos
+  repos $@
   # Install the driver
   sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
 }
 sqlcmd() {
-  repos
+  repos $@
   # optional: for bcp and sqlcmd
   sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
   echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >>~/.bashrc
