12f345a337699e80b1e510c1a0fb8122179f4a9e
David Liu <david-khala@hotmail.com>
2024-11-18 11:33:28 +0800

    Update python.sh

--- language/python.sh
+++ language/python.sh
@@ -2,8 +2,11 @@
 set-python() {
     sudo apt install -y python-is-python3
 }
-install-pip() {
+pip() {
     sudo apt install -y python3-pip
+}
+poetry(){
+    sudo apt install -y python3-poetry
 }
 
 $@
