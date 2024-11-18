57abcd3e7cec6fb2afd7b799997979a99232d2dd
David Liu <david-khala@hotmail.com>
2024-11-18 10:32:19 +0800

    Update python.sh

--- language/python.sh
+++ language/python.sh
@@ -1,18 +1,9 @@
-#!/usr/bin/env bash
 
 setPython3() {
     python-is-python3
 }
-installPIP3() {
+installPIP() {
     sudo apt install python3-pip
 }
-installPIP2() {
-    sudo apt install python-pip
-}
-installDjango() {
-    pip3 install django
-}
-installPython2() {
-    sudo apt install python
-}
-$1
+
+$@
