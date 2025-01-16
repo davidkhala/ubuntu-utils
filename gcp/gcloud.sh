8e163264cc08cbf27ac73c7fb82cd1e96a5a019d
David Liu <david-khala@hotmail.com>
2025-01-16 11:09:25 +0800

    Update gcloud.sh

--- gcp/gcloud.sh
+++ gcp/gcloud.sh
@@ -1,5 +1,5 @@
 set -e
-setup(){
+install() {
   ## https://cloud.google.com/sdk/docs/install#deb
   echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
   sudo apt-get install -y apt-transport-https ca-certificates gnupg
@@ -7,4 +7,4 @@ setup(){
   sudo apt-get update && sudo apt-get install -y google-cloud-sdk
 
 }
-$1
+"$@"
