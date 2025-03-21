402b4cda37c5709fd5f39bbe291d448d4b2171f2
David Liu <david-khala@hotmail.com>
2025-03-21 14:42:57 +0800

    Update backstage.sh

--- apps/backstage.sh
+++ apps/backstage.sh
@@ -1,11 +1,15 @@
 set -e
 prepare(){
-      sudo apt install -y git make build-essential nodejs npm curl
-    sudo npm install -g corepack
+      sudo apt install -y curl git make build-essential
+      curl -fsSL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+      sudo -E bash nodesource_setup.sh
+      sudo apt install -y nodejs 
+      rm bash nodesource_setup.sh
+      
+    
 
     curl -sSL https://raw.githubusercontent.com/davidkhala/ubuntu-utils/refs/heads/master/apps/docker.sh | bash -s install
-    echo Y | yarn --version
-    
+        
     sudo apt install -y zlib1g-dev libc-ares-dev libbrotli-dev libnghttp2-dev libicu-dev # for yarn install
 
 }
