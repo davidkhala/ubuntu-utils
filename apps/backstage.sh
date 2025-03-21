6266148ba25f30930ce4a892b756086264bde256
David Liu <david-khala@hotmail.com>
2025-03-21 07:46:47 +0000

    add backstage

--- apps/backstage.sh
+++ apps/backstage.sh
@@ -4,7 +4,7 @@ prepare() {
   curl -fsSL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
   sudo -E bash nodesource_setup.sh
   sudo apt install -y nodejs
-  rm bash nodesource_setup.sh
+  rm nodesource_setup.sh
   curl -sSL https://raw.githubusercontent.com/davidkhala/ubuntu-utils/refs/heads/master/apps/docker.sh | bash -s install
   sudo apt install -y zlib1g-dev libc-ares-dev libbrotli-dev libnghttp2-dev libicu-dev # for yarn install
 }
