da90cd9b339c325012ae07cbc5a3c6a3a39fc7c3
David Liu <david-khala@hotmail.com>
2025-04-18 20:08:51 +0800

    Update docker.sh

--- apps/docker.sh
+++ apps/docker.sh
@@ -22,8 +22,7 @@ add-distro-repository() {
   # Add the repository to Apt sources:
   echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
   sudo apt-get update
 
 }
