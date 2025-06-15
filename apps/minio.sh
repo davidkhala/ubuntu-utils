3d02864125bbcc602edd4836dc3884dc69f5b172
David Liu <david-khala@hotmail.com>
2025-06-15 15:23:20 +0000

    fix ci

--- apps/minio.sh
+++ apps/minio.sh
@@ -1,18 +1,13 @@
 set -e
-user=${2:-admin}
-password=${3:-password}
-path=${4:-/mnt/data}
+# for `mc` CLI install, see https://raw.githubusercontent.com/davidkhala/linux-utils/refs/heads/main/apps/minio.sh
 install-server() {
-  wget https://dl.min.io/server/minio/release/linux-amd64/minio_20221021223748.0.0_amd64.deb
-  sudo dpkg -i minio_20221021223748.0.0_amd64.deb
-  rm minio_20221021223748.0.0_amd64.deb
+  wget https://dl.min.io/server/minio/release/linux-amd64/archive/minio_20250524170830.0.0_amd64.deb -O minio.deb
+  sudo dpkg -i minio.deb
+  rm minio.deb
+  local user=${1:-admin}
+  local password=${2:-password}
+  local path=${3:-/mnt/data}
   MINIO_ROOT_USER=$user MINIO_ROOT_PASSWORD=$password minio server $path --console-address ":9001" &
 }
-install-client() {
-  wget https://dl.min.io/client/mc/release/linux-amd64/mcli_20221020232633.0.0_amd64.deb
-  sudo dpkg -i mcli_20221020232633.0.0_amd64.deb
-  rm mcli_20221020232633.0.0_amd64.deb
-  #   TODO
-  mcli alias set myminio/ http://MINIO-SERVER MYUSER MYPASSWORD
-}
+
 $@
