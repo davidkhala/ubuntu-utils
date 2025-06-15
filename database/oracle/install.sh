2b56e1bcb4442f2549c6bb7add7a8f55f0e21240
David Liu <david-khala@hotmail.com>
2025-06-15 22:50:38 +0000

    close #2

--- database/oracle/install.sh
+++ database/oracle/install.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo apt install -y alien
 SQLDeveloper() {
-    wget https://objectstorage.ap-seoul-1.oraclecloud.com/n/cn9yc2hk0gzg/b/Oracle-Database-Development/o/sqldeveloper-20.4.0.379.2205-20.4.0-379.2205.noarch.rpm
+    wget https://download.oracle.com/otn_software/java/sqldeveloper/sqldeveloper-24.3.1-347.1826.noarch.rpm
     sudo alien -i --scripts sqldeveloper-*.rpm
     rm sqldeveloper-*.rpm
 
