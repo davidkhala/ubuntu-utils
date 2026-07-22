9465f1046aee914dd8ec4e1297d97e5862a0e27b
David Liu <david-khala@hotmail.com>
2026-07-22 21:54:39 +0800

    no cron

--- database/mssql/install.sh
+++ database/mssql/install.sh
@@ -1,32 +1,33 @@
 repos() {
+  local VERSION_ID=$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2)
 
-  local isLegacy=$1
-  if [[ "--isLegacy" == $isLegacy ]]; then
-    # For Ubuntu versions < 24.04
-    curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-  else
-    # For Ubuntu versions >= 24.04
-    curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
-
+  if ! [[ "18.04 20.04 22.04 24.04 25.10" == *"$VERSION_ID"* ]]; then
+    echo "Ubuntu $VERSION_ID is not currently supported."
+    exit 1
   fi
 
-  # Add repo to apt sources
-  curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+  # Download the package to configure the Microsoft repo
+  curl -sSL -O https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+  sudo dpkg -i packages-microsoft-prod.deb
+  rm packages-microsoft-prod.deb
 
   sudo apt-get update
 }
+
 odbc() {
-  repos $@
+  repos
   # Install the driver
   sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+  # optional: for unixODBC development headers
+  sudo apt-get install -y unixodbc-dev
 }
+
 sqlcmd() {
-  repos $@
+  repos
   # optional: for bcp and sqlcmd
   sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
-  echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >>~/.bashrc
+  echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
   source ~/.bashrc
-
 }
 
 $@
