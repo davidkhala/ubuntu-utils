1ba0ed8e6cfa6ce8f003d1996d4bb6e60c4d57d4
David Liu <david-khala@hotmail.com>
2026-07-23 21:54:20 +0800

    Update install.sh
    add 17 support

--- database/mssql/install.sh
+++ database/mssql/install.sh
@@ -1,33 +1,60 @@
 repos() {
-  local VERSION_ID=$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2)
+	local VERSION_ID=$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2)
 
-  if ! [[ "18.04 20.04 22.04 24.04 25.10" == *"$VERSION_ID"* ]]; then
-    echo "Ubuntu $VERSION_ID is not currently supported."
-    exit 1
-  fi
+	if ! [[ "18.04 20.04 22.04 24.04 25.10" == *"$VERSION_ID"* ]]; then
+		echo "Ubuntu $VERSION_ID is not currently supported."
+		exit 1
+	fi
 
-  # Download the package to configure the Microsoft repo
-  curl -sSL -O https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
-  sudo dpkg -i packages-microsoft-prod.deb
-  rm packages-microsoft-prod.deb
+	# Download the package to configure the Microsoft repo
+	curl -sSL -O https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+	sudo dpkg -i packages-microsoft-prod.deb
+	rm packages-microsoft-prod.deb
 
-  sudo apt-get update
+	sudo apt-get update
 }
 
 odbc() {
-  repos
-  # Install the driver
-  sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
-  # optional: for unixODBC development headers
-  sudo apt-get install -y unixodbc-dev
+	local version=${1:-18}
+
+	repos
+
+	case "$version" in
+	18)
+		sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+		;;
+	17)
+		sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
+		;;
+	*)
+		echo "Unsupported ODBC version: $version."
+		exit 1
+		;;
+	esac
+	# optional: for unixODBC development headers
+	sudo apt-get install -y unixodbc-dev
 }
 
 sqlcmd() {
-  repos
-  # optional: for bcp and sqlcmd
-  sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
-  echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
-  source ~/.bashrc
+	repos
+	local version=${1:-18}
+
+	case "$version" in
+	18)
+		sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+		echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >>~/.bashrc
+		;;
+	17)
+		sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
+		echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >>~/.bashrc
+		;;
+	*)
+		echo "Unsupported ODBC version: $version."
+		exit 1
+		;;
+	esac
+
+	source ~/.bashrc
 }
 
 $@
